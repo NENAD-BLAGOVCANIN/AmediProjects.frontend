0dda85f65259040df819f86c269d85dde089a112
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-04-05 20:32:19 +0200

    updates

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,29 +2,32 @@ import React from 'react'
 
 function Projects() {
     return (
-        <div className='container w-100 h-100 d-flex justify-content-center flex-column' style={{ minHeight: '100vh' }}>
-            <h5 className='mb-4 ps-2'>Your Firebase projects</h5>
-            <div className="row">
-                <div className="col-md-3 px-3">
-                    <div className="card border-0 bg-gray shadow-sm py-5 w-100 d-flex align-items-center justify-content-center">
-                        <h1>+</h1>
-                        <span className='fw-500'>Add Project</span>
+        <div id='create-project-page-wrapper'>
+            <div className='container w-100 h-100 d-flex justify-content-center flex-column' style={{ minHeight: '100vh' }}>
+                <h5 className='mb-4 ps-2'>Existing Amedi projects</h5>
+                <div className="row">
+                    <div className="col-md-3 px-3">
+                        <div className="project-card p-4 card border-0 bg-gray bg-gray-hover pointer shadow-sm w-100 d-flex align-items-center justify-content-center">
+                            <h1>+</h1>
+                            <span className='fw-500'>Add Project</span>
+                        </div>
                     </div>
-                </div>
-                <div className="col-md-3 px-3">
-                    <div className="card border-0 bg-gray shadow-sm py-5 w-100 d-flex align-items-center justify-content-center">
-                        <h1>+</h1>
-                        <span className='fw-500'>Add Project</span>
+                    <div className="col-md-3 px-3">
+                        <div className="project-card p-4 card border-0 bg-gray bg-gray-hover pointer shadow-sm w-100 d-flex">
+                            <h5 className='fw-500'>Example Project 1</h5>
+                            <span className='text-muted small'>This is a random project description</span>
+                        </div>
                     </div>
-                </div>
-                <div className="col-md-3 px-3">
-                    <div className="card border-0 bg-gray shadow-sm py-5 w-100 d-flex align-items-center justify-content-center">
-                        <h1>+</h1>
-                        <span className='fw-500'>Add Project</span>
+                    <div className="col-md-3 px-3">
+                        <div className="project-card p-4 card border-0 bg-gray bg-gray-hover pointer shadow-sm w-100 d-flex">
+                            <h5 className='fw-500'>Example Project 2</h5>
+                            <span className='text-muted small'>This is a random project description</span>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
