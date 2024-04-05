f29c5faf6486c1dcb727e5893650505d625a9a58
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-04-05 23:39:30 +0200

    updates

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,43 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowTrendUp } from '@fortawesome/free-solid-svg-icons';
-import { getStats } from '../api/dashboard';
-import { Pie, Doughnut } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-
-ChartJS.register(ArcElement, Tooltip, Legend);
+import { Chart, ArcElement, Tooltip, Legend } from 'chart.js/auto';
+import { Pie, Doughnut, Line } from 'react-chartjs-2';
 
 function Dashboard() {
-  const [stats, setStats] = useState({
-    contactCount: 0,
-    leadCount: 0,
-    taskCount: 0,
-    todoTasksCount: 0,
-    inProgressTasksCount: 0,
-    doneTasksCount: 0,
+  const [stats] = useState({
+    contactCount: Math.floor(Math.random() * 100),
+    leadCount: Math.floor(Math.random() * 100),
+    taskCount: Math.floor(Math.random() * 100),
+    todoTasksCount: Math.floor(Math.random() * 100),
+    inProgressTasksCount: Math.floor(Math.random() * 100),
+    doneTasksCount: Math.floor(Math.random() * 100),
   });
-
-  useEffect(() => {
-    const fetchStats = async () => {
-      try {
-        const fetchedStats = await getStats();
-        setStats(fetchedStats);
-      } catch (error) {
-        console.error('Error fetching:', error);
-      }
-    };
-
-    fetchStats();
-  }, []);
 
   const contactData = {
     labels: ['Contacts', 'Leads'],
     datasets: [
       {
         label: '',
-        data: [stats.contactCount, stats.leadCount],
+        data: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
         backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
         borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
         borderWidth: 1,
@@ -50,7 +34,11 @@ function Dashboard() {
     datasets: [
       {
         label: '',
-        data: [stats.todoTasksCount, stats.inProgressTasksCount, stats.doneTasksCount],
+        data: [
+          Math.floor(Math.random() * 100),
+          Math.floor(Math.random() * 100),
+          Math.floor(Math.random() * 100),
+        ],
         backgroundColor: ['#B4EBFF', '#EBFFB4', '#B4FFD2'],
         borderColor: ['#246A84', '#98AC60', '#5EAB7D'],
         borderWidth: 1,
@@ -58,6 +46,18 @@ function Dashboard() {
     ],
   };
 
+  const lineChartData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    datasets: [
+      {
+        label: 'Random Data',
+        data: Array.from({ length: 12 }, () => Math.floor(Math.random() * 100)),
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1,
+      },
+    ],
+  };
 
   return (
     <div className="main-content-wrapper">
@@ -68,7 +68,7 @@ function Dashboard() {
           <div className="row">
             <div className="col-md-4 p-3">
               <div className="card">
-                <span className="small">Contacts</span>
+                <span className="small">Projects Completed</span>
                 <div className="d-flex align-items-center">
                   <h2 className="m-0 pe-2">{stats.contactCount}</h2>
                   <FontAwesomeIcon icon={faArrowTrendUp} className="text-success" />
@@ -77,7 +77,7 @@ function Dashboard() {
             </div>
             <div className="col-md-4 p-3">
               <div className="card">
-                <span className="small">Leads</span>
+                <span className="small">Active Clients</span>
                 <div className="d-flex align-items-center">
                   <h2 className="m-0 pe-2">{stats.leadCount}</h2>
                   <FontAwesomeIcon icon={faArrowTrendUp} className="text-success" />
@@ -86,9 +86,9 @@ function Dashboard() {
             </div>
             <div className="col-md-4 p-3">
               <div className="card">
-                <span className="small">Tasks</span>
+                <span className="small">Monthy Earnings</span>
                 <div className="d-flex align-items-center">
-                  <h2 className="m-0 pe-2">{stats.taskCount}</h2>
+                  <h2 className="m-0 pe-2">${stats.taskCount}K</h2>
                   <FontAwesomeIcon icon={faArrowTrendUp} className="text-success" />
                 </div>
               </div>
@@ -108,6 +108,16 @@ function Dashboard() {
                 <h5 className="mb-3">Task Management</h5>
                 <div className="m-auto" style={{ maxWidth: 400 }}>
                   <Doughnut data={taskCompletionData} />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12 p-3">
+              <div className="card w-100">
+                <h5 className="mb-3">Monthly Earnings</h5>
+                <div className="m-auto w-100 h-100 d-flex justify-content-center text-center" style={{maxWidth: 1200}}>
+                  <Line data={lineChartData} />
                 </div>
               </div>
             </div>
