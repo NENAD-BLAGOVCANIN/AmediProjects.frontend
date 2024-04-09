70c4c99938b4fb13429b0010aa57ac60ac87a696
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-04-09 15:56:17 +0200

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -69,7 +69,7 @@ function Header({ pageTitle }) {
                             </Link>
                             <hr className='m-0 py-2' />
                             {myProjects.map(myProject => (
-                                <a className="dropdown-item py-2" key={myProject.id} href="#">{myProject.name}</a>
+                                <span className="dropdown-item py-2 pointer" key={myProject.id} onClick={() => {handleSwitchProject(myProject.id)}}>{myProject.name}</span>
                             ))}
                         </div>
                     </div>
