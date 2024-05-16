6af361451f7d823d2db9f45c47ead474bed3f95b
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-05-16 12:50:32 +0200

    home page updates

--- src/pages/project/Home.jsx
+++ src/pages/project/Home.jsx
@@ -6,13 +6,25 @@ import MyCalendar from '../../components/MyCalendar';
 import profileImagePlaceholder from '../../assets/img/profile.svg'
 import placeholderProfileImage1 from '../../assets/img/placeholder-profile-img-1.jpeg'
 import placeholderProfileImage2 from '../../assets/img/placeholder-profile-img-2.jpg'
+import projectPlaceholderIcon from '../../assets/img/projectPlaceholderIcon.jpg'
 import { getProjectInfo } from '../../api/project';
+import amediProfileImg from '../../assets/img/amediProfileImg.webp'
 
 function Home() {
 
     const [projectInfo, setProjectInfo] = useState([]);
+    const [userInfo, setUserInfo] = useState(null);
 
     useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                const fetchedUserInfo = await getUserInfo();
+                setUserInfo(fetchedUserInfo);
+                console.log(fetchedUserInfo);
+            } catch (error) {
+                console.error("Error fetching :", error);
+            }
+        }
         const fetchProjectInfo = async () => {
             try {
                 const fetchedProjectInfo = await getProjectInfo();
@@ -22,6 +34,7 @@ function Home() {
             }
         };
 
+        fetchUserInfo();
         fetchProjectInfo();
 
     }, []);
@@ -31,34 +44,64 @@ function Home() {
         <div className="container">
 
             <div className='pt-3'>
-                <h1 style={{ fontSize: '40pt' }}>
-                    🏡
-                </h1>
-                <h2 className='bold mt-3 mb-4'>Project Home</h2>
+                <h3 className='mt-3 mb-4 fw-500'>Welcome, {userInfo && userInfo.name.split(" ")[0]}!</h3>
 
-                <span className='badge badge-primary bg-warning fw-400'>Status: <span className='fw-500'>{projectInfo.status}</span></span>
 
-                <div className='d-flex align-items-center pt-3 pb-5'>
-                    <span className='fw-500'>Team:</span>
-                    <div className='rounded bg-gray px-2 py-1 small mx-2'>
-                        <span className='pe-2 small'>Wanda</span>
-                        <img src={profileImagePlaceholder} className='rounded-circle' alt="" style={{ maxHeight: 25, height: '100%' }} />
-                    </div>
-                    <div className='rounded bg-gray px-2 py-1 small mx-2'>
-                        <span className='pe-2 small'>Wanda</span>
-                        <img src={placeholderProfileImage1} className='rounded-circle' alt="" style={{ maxHeight: 25, height: '100%' }} />
-                    </div>
-                    <div className='rounded bg-gray px-2 py-1 small mx-2'>
-                        <span className='pe-2 small'>Wanda</span>
-                        <img src={placeholderProfileImage2} className='rounded-circle' alt="" style={{ maxHeight: 25, height: '100%' }} />
+            </div>
+
+            <div className="row pt-5">
+                <div className="col-md-6">
+                    <div className="bg-white rounded p-3 shadow-sm">
+                        <h6 className='bold mb-3'>Project overview</h6>
+                        <div className='d-flex'>
+                            <img
+                                src={projectInfo?.image ?? projectPlaceholderIcon}
+                                className="rounded"
+                                style={{ height: 55, width: 55, objectFit: "cover" }}
+                                alt=""
+                            />
+                            <div className='ps-3 d-flex flex-column'>
+                                <span className='fw-500'>{projectInfo.name}</span>
+                                <span className='small text-muted pb-3 pt-1'>{projectInfo.description}</span>
+                                <span className='badge badge-primary bg-warning fw-400'>Status: <span className='fw-500'>{projectInfo.status}</span></span>
+                            </div>
+
+                        </div>
+
+
                     </div>
                 </div>
+                <div className="col-md-6">
+                    <div className="bg-white rounded p-3 shadow-sm">
+                        <h6 className='bold mb-3'>Alerts</h6>
 
-                <p className='text-muted medium pt-2' style={{ textAlign: 'justify' }}>
-                    Give your colleagues a place to learn about your team, and what you’re working on. Use the + buttons in your left-hand sidebar to add more pages, like process docs or a project roadmap.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam deleniti debitis, magnam voluptate unde ad, quos, error maxime suscipit tenetur et. Placeat officiis, quibusdam quo voluptatem unde deleniti accusamus eligendi.
-                    Give your colleagues a place to learn about your team, and what you’re working on.
-                </p>
+                        <ul className="custom-notifications">
+                            {userInfo.notifications.map((notification) => (
+                                <li className="unread" key={notification.id}>
+                                    <a href="#" className="d-flex">
+                                        <div className="notification-sender-icon me-3">
+                                            <img
+                                                src={amediProfileImg}
+                                                alt="Image"
+                                                className="img-fluid"
+                                            />
+                                        </div>
+                                        <div className="text">
+                                            <strong>{notification.title}</strong>
+                                            <p className="small pt-1">{notification.body}</p>
+                                        </div>
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                        <p className="text-center m-0 p-0">
+                            <a href="/notifications" className="small">
+                                View All
+                            </a>
+                        </p>
+
+                    </div>
+                </div>
             </div>
 
             <div className="row pt-5">
